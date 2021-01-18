c1b815ce908e4f7c566808b507c3d990e4148352
Christian Cotignola <christiancotignola@gmail.com>
2021-01-18 17:56:46 +0100

    Update admin_mail_sender.php

--- cron_tasks/admin_mail_sender.php
+++ cron_tasks/admin_mail_sender.php
@@ -2,29 +2,7 @@
 chdir('/home/biblioteve');
 require_once('www/libs/config.php');
 require_once('www/libs/misc.php');
-/*
-define("DB_SERVER", "bibliotevebiblio.mysql.db"); 
-define("DB_USER", "bibliotevebiblio"); 
-define("DB_PASS", "NuPNruL9R7m8Jx"); 
-define("DB_NAME", "bibliotevebiblio");
-define("MAIL_SENDER_FROM", 'Mailing System bibliotecapacinotti <system@bibliotecapacinotti.it>');
 
-
-
-function send_mail(string $to, string $subject, string $content): bool{
-	$headers=['From' => MAIL_SENDER_FROM,
-			  'Content-Type' => 'text/html; charset="UTF-8"'
-			  ];
-	return mail($to, $subject, $content, $headers); 
-}
-
-function get_database_instance() : object{
-	mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-	$db=new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
-	$db->set_charset("utf8");
-	return $db;
-}
-*/
 
 $db=get_database_instance();
 $res=$db->query("SELECT COUNT(*) FROM richieste WHERE rifiutato=0 AND validato=1 AND id_richiesta NOT IN(SELECT id_richiesta FROM prestiti)");
